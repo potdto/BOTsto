8e6db8278d4db331c4179e623f3284183c901352
potdto <potstooo@gmail.com>
2022-05-06 16:44:33 +0100

    added APL calculate function

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-require("dotenv").config();
-const tmi = require('tmi.js');
+import dotenv from "dotenv";
+dotenv.config();
+import { Client } from 'tmi.js';
+import fetch from "node-fetch";
 
-const client = new tmi.Client({
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+async function executeAPL(code) {
+    const res = await fetch("https://tryapl.org/Exec", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify(['', 0, '', code]),
+    });
+    const data = await res.json();
+    return data[3];
+}
+
+
+const client = new Client({
     options: { debug: true },
     identity: {
         username: process.env.USERNAME,
@@ -12,10 +26,38 @@ const client = new tmi.Client({
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
+client.on('message', async (channel, tags, message, self) => {
     if (self) return;
+    if (message.match(/^i'?m/)) {
+        client.say(channel, "Hi " + message.split` `.slice(1).join` ` + ", I'm botsto!");
+    }
 
-    if (message.match(/^i'?m/i)) {
-        client.say(channel, "Hi " + message.split` `.slice(1).join` `+", I'm botsto!");
+    if (message == "!ping")
+        client.say(channel, "pong");
+
+    if (message == "!8ball")
+        client.say(channel,
+            ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."][Math.floor(Math.random() * 20)]
+        );
+
+    if (message.startsWith("!pyramid ")) {
+        const argument = message.split(" ")[1];
+
+        client.say(channel, argument);
+        await delay(1000);
+        client.say(channel, argument + " " + argument);
+        await delay(1000);
+        client.say(channel, argument + " " + argument + " " + argument);
+        await delay(1000);
+        client.say(channel, argument + " " + argument);
+        await delay(1000);
+        client.say(channel, argument);
+
+    }
+    if (message.startsWith("!calc ")) {
+        const argument = message.split(" ")[1];
+        client.say(channel,
+                String(await executeAPL(argument))
+            );
     }
 });
